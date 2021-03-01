265c7ac0fab3b4e262e5d9b031b8823aba4955e1
Martin Duy Tat <martin.tat@physics.ox.ac.uk>
2021-03-01 12:10:29 +0000

    Implemented a constructor for BDTVariables

--- src/BDTVariables.cpp
+++ src/BDTVariables.cpp
@@ -5,6 +5,7 @@
 #include<vector>
 #include<map>
 #include<sstream>
+#include<fstream>
 
 std::string BDTVariables::DetermineTransformation(std::string Expression) {
   if(Expression.substr(0, 7) == "log(1 -") {
@@ -25,7 +26,7 @@ std::string BDTVariables::DetermineTransformation(std::string Expression) {
     variable1.erase(variable1.size() - 1);
     // Remove last 2 characters of second part
     std::string variable2 = SecondPart.erase(SecondPart.size() - 2);
-    return "log(min( " + variable1 + " " + variable2;
+    return "log(min(x, y)) " + variable1 + " " + variable2;
   } else if(Expression.substr(0, 8) == "log(max(") {
     // If transformation is "log(max(x, y))
     // First split up string by whitespace
@@ -38,7 +39,7 @@ std::string BDTVariables::DetermineTransformation(std::string Expression) {
     variable1.erase(variable1.size() - 1);
     // Remove last 2 characters of second part
     std::string variable2 = SecondPart.erase(SecondPart.size() - 2);
-    return "log(max( " + variable1 + " " + variable2;
+    return "log(max(x, y)) " + variable1 + " " + variable2;
   } else if(Expression.substr(0, 4) == "log(") {
     // If transformation is "log(x)
     // Remove first 4 characters and last character to get varables name
@@ -52,3 +53,24 @@ std::string BDTVariables::DetermineTransformation(std::string Expression) {
 }
 
 BDTVariables::BDTVariables(std::string Filename) {
+  std::ifstream TrainingVariablesFiles(Filename);
+  if(TrainingVariablesFile.is_open()) {
+    std::string line;
+    while(std::getline(TrainingVariablesFile, line)) {
+      std::string ParsedLine = DetermineTransformation(line);
+      std::string transformation;
+      std::istringstream ss(ParsedLine);
+      ss >> transformation;
+      if(transformation == "None" || transformation == "log(1 - x)" || transformation == "log(x)") {
+	std::string variable;
+	ss >> variable;
+	m_Variables.push_back(std::vector<std::string>{transformation, line, variable});
+      } else if(transformation == "log(min(x, y))" || transformation == "log(max(x, y))") {
+	std::string variable1, variable2;
+	ss >> variable1;
+	ss >> variable2;
+	m_Variables.push_back(std::vector<std::string>{transformation, line, variable1, variable2});
+      }
+    }
+  }
+}
